95e4be2033aeb6689362dde083de081eee5bc6f8
Carsten Lehrmann <carsten-lehrmann@outlook.de>
2026-04-12 19:19:25 +0200

    init: Initial commit

---
+++ janisty/janisty/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,7 @@
+namespace janisty.Components.Account;
+
+public class PasskeyInputModel
+{
+    public string? CredentialJson { get; set; }
+    public string? Error { get; set; }
+}
---
+++ janisty/janisty/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,7 @@
+namespace janisty.Components.Account;
+
+public enum PasskeyOperation
+{
+    Create = 0,
+    Request = 1,
+}
